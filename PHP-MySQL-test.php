0b67c7ed076ced9b5eb4add58dda9278808bd558
root <root@vladimir-Satellite-C655.(none)>
2013-02-24 19:07:35 -0500

    added the following queries: selecting status, profession, zip code data

--- PHP-MySQL-test.php
+++ PHP-MySQL-test.php
@@ -81,6 +81,54 @@ for($j = 0; $j < $rows_tc_int; $j++)
 }
 echo '</table><br /><br />';
 
+// retrieving contacts status
+$query = mysql_query(
+					"SELECT tc.last_name, st.status FROM team_contacts " . 
+					"AS tc INNER JOIN status AS st ON tc.status_id=st.status_id"
+					);
+if(!$query) die("Unable to access database: " . mysql_error());
+
+echo '<table border="1"><tr><th>Last Name</th><th>Status</th></tr>';
+$rows_tc = mysql_num_rows($query);
+for($i = 0; $i < $rows_tc; $i++)
+{
+	$row_tc = mysql_fetch_row($query);
+	echo "<tr><td>$row_tc[0]</td><td>$row_tc[1]</td></tr>";
+}
+echo '</table><br /><br />';
+
+// retrieving contacts profession
+$query = mysql_query(
+					"SELECT tc.last_name, prof.profession FROM team_contacts AS tc " .
+					"INNER JOIN profession AS prof ON tc.prof_id=prof.prof_id;"
+					);
+if(!$query) die("Unable to access database: " . mysql_error());
+
+echo '<table border="1"><tr><th>Last Name</th><th>Profession</th></tr>';
+$rows_tc = mysql_num_rows($query);
+for($i = 0; $i < $rows_tc; $i++)
+{
+	$row_tc = mysql_fetch_row($query);
+	echo "<tr><td>$row_tc[0]</td><td>$row_tc[1]</td></tr>";
+}
+echo '</table><br /><br />';
+
+// retrieving contacts zip_code
+$query = mysql_query(
+					"SELECT tc.last_name, zc.zip_code, zc.city, zc.state FROM team_contacts " . 
+					"AS tc INNER JOIN zip_code AS zc ON tc.zip_code=zc.zip_code"
+					);
+if(!$query) die("Unable to access database: " . mysql_error());
+
+echo '<table border="1"><tr><th>Last Name</th><th>Zip Code</th><th>City</th><th>State</th></tr>';
+$rows_tc = mysql_num_rows($query);
+for($i = 0; $i < $rows_tc; $i++)
+{
+	$row_tc = mysql_fetch_row($query);
+	echo "<tr><td>$row_tc[0]</td><td>$row_tc[1]</td><td>$row_tc[2]</td><td>$row_tc[3]</td></tr>";
+}
+echo '</table><br /><br />';
+
 echo '</body></html>';
 
 function sanitizeString($var)
@@ -100,3 +148,4 @@ function sanitizeMySQL($var)
 }
 	
 ?>
+
